cf5e8268b3eff4c434dc1b7c25657798977c40f7
Dmitri Ponomarev <sidey383@mail.ru>
2025-11-11 18:05:37 +0300

    Add constructor for ExternalPrivateChatMessageEvent

--- Core/src/main/java/ru/hackaton/chatsync/event/ExternalPrivateChatMessageEvent.java
+++ Core/src/main/java/ru/hackaton/chatsync/event/ExternalPrivateChatMessageEvent.java
@@ -1,9 +1,11 @@
 package ru.hackaton.chatsync.event;
 
 import lombok.Getter;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.hackaton.chatsync.ExternalUser;
 
 @Getter
@@ -12,6 +14,11 @@ public class ExternalPrivateChatMessageEvent extends ExternalChatMessageEvent {
     private static final HandlerList HANDLER_LIST = new HandlerList();
 
     private final Player player;
+
+    public ExternalPrivateChatMessageEvent(@NotNull Player player, @Nullable OfflinePlayer userPlayer, @NotNull ExternalUser user, @NotNull String message) {
+        super(userPlayer, user, message);
+        this.player = player;
+    }
 
     public ExternalPrivateChatMessageEvent(@NotNull Player player, @NotNull ExternalUser user, @NotNull String message) {
         super(user, message);
